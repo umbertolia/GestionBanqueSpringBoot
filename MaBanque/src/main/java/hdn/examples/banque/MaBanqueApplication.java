d6102951beeaa28e0b0c24b43f4492efee59bf3e
Administrator <Administrator@192.168.1.5>
2018-12-08 00:45:17 +0100

    maj controller

--- MaBanque/src/main/java/hdn/examples/banque/MaBanqueApplication.java
+++ MaBanque/src/main/java/hdn/examples/banque/MaBanqueApplication.java
@@ -27,7 +27,11 @@ public class MaBanqueApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		logger.info("Appli MaBanque lancée");
+		logger.info("Appli MaBanque lancée");
+		
 		
 			
+		
+		
 	}
 }
